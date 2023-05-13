98e3f368a0388caa7dc0bf7c7e86097d3ff88062
wzz <bean_jie@aol.com>
2023-05-13 17:04:15 +0800

    立项

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '瀚海打印模块',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
---
+++ src/views/print/PrintPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+.page{
+      position: relative;
+      width: 864px;
+      height: 624px;
+      margin: 20% auto;
+      background: #fff url(http://81.68.204.177/img/1-1.png) no-repeat center center;
+    }
+</style>
